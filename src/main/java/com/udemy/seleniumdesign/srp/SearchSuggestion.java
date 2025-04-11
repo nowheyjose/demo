99a26ca46c01bbd99bd1b3b10084cc490bcc4e22
Jose <jdiaz5511@gmail.com>
2025-04-11 14:29:51 -0700

    SDET Design patterns project

---
+++ src/main/java/com/udemy/seleniumdesign/srp/SearchSuggestion.java
@@ -0,0 +1,10 @@
+package com.udemy.seleniumdesign.srp;
+
+public class SearchSuggestion extends AbstractComponent {
+
+    @Override
+    public boolean isDisplayed() {
+        return false;
+    }
+
+}
